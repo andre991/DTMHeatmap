60dbec6209573086345f978cd0d3b8e5abdaf7a1
André  Santana Ferreira <andre.991@hotmail.com>
2017-03-14 19:41:42 -0300

    Carthage Compatible

---
+++ DTMHeatmap/DTMHeatmap.h
@@ -0,0 +1,19 @@
+//
+//  DTMHeatmap.h
+//  DTMHeatmap
+//
+//  Created by Andre Santana Ferreira on 14/03/17.
+//  Copyright © 2017 Bryan Oltman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTMHeatmap.
+FOUNDATION_EXPORT double DTMHeatmapVersionNumber;
+
+//! Project version string for DTMHeatmap.
+FOUNDATION_EXPORT const unsigned char DTMHeatmapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTMHeatmap/PublicHeader.h>
+
+
